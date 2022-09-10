f8032dbdb488adee54a900669d0c78a6543a847a
commandf1 <mkhjmy@qq.com>
2022-09-10 09:34:35 +0800

    update

--- src/main/java/space/commandf1/FasterLib/commands/FasterLibCommand.java
+++ src/main/java/space/commandf1/FasterLib/commands/FasterLibCommand.java
@@ -3,6 +3,8 @@ package space.commandf1.FasterLib.commands;
 import org.bukkit.command.CommandSender;
 import space.commandf1.FasterLib.FasterLib;
 import space.commandf1.FasterLib.api.command.Command;
+import space.commandf1.FasterLib.api.inventory.action.GUIAction;
+import space.commandf1.FasterLib.listeners.GUIListener;
 
 public class FasterLibCommand extends Command {
 
@@ -14,8 +16,17 @@ public class FasterLibCommand extends Command {
     public boolean onExecute(CommandSender commandSender, org.bukkit.command.Command cmd, String label, String[] args) {
         commandSender.sendMessage(new String[] {
                 "§fFasterLib " + "§fBy §b" + "commandf1",
-                "§aVersion §f-> §c" + FasterLib.getInstance().getDescription().getVersion()
+                "§aVersion §f-> §c" + FasterLib.getInstance().getDescription().getVersion(),
+                "",
+                "§fGUIActions §f-> §b" + GUIListener.actions.keySet().size(),
+                "§fActions §f-> §b" + GUIListener.actions.size(),
+                "§fInventories §f-> §b" + GUIListener.actions.values().size(),
+                "",
+                "§aGUIAction Info"
         });
+        for (GUIAction guiAction : GUIListener.actions.keySet()) {
+            commandSender.sendMessage("§f" + GUIListener.actions.get(guiAction).getTitle() + " §f-> " + "§b" + guiAction.getAction().toString());
+        }
         return true;
     }
 }
